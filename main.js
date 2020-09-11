43d785c54fb729c9730fdff695f5796dc43025b2
dhall13-ctl <derek.k.hall@centurylink.com>
2020-09-11 21:53:00 +0000

    Lab 4

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 /*
- Import the built-in path module.
- See https://nodejs.org/api/path.html
- The path module provides utilities for working with file and directory paths.
- IAP requires the path module to access local file modules.
- The path module exports an object.
- Assign the imported object to variable path.
+  Import the ip-cidr npm package.
+  See https://www.npmjs.com/package/ip-cidr
+  The ip-cidr package exports a class.
+  Assign the class definition to variable IPCIDR.
+*/
+const IPCIDR = require('ip-cidr');
+/*
+  Import the built-in path module.
+  See https://nodejs.org/api/path.html
+  The path module provides utilities for working with file and directory paths.
+  IAP requires the path module to access local file modules.
+  The path module exports an object.
+  Assign the imported object to variable path.
 */
 const path = require('path');
 
@@ -13,25 +20,33 @@ const path = require('path');
  * as this module. IAP requires the path object's join method
  * to unequivocally locate the file module.
  */
-const { getIpv4MappedIpv6Address } = require(path.join(__dirname, './ipv6.js'));
- 
-/*
-  Import the ip-cidr npm package.
-  See https://www.npmjs.com/package/ip-cidr
-  The ip-cidr package exports a class.
-  Assign the class definition to variable IPCIDR.
-*/
-const IPCIDR = require('ip-cidr');
+const { getIpv4MappedIpv6Address } = require(path.join(__dirname, 'ipv6.js'));
+/**
+ * Calculate and return the first host IP address from a CIDR subnet.
+ * @param {string} cidrStr - The IPv4 subnet expressed
+ *                 in CIDR format.
+ * @param {callback} callback - A callback function.
+ * @return {string} (firstIpAddress) - An IPv4 address.
+ */
 
 
 
+/**
+ * Calculates an IPv4-mapped IPv6 address.
+ * @param {string} ipv4 - An IPv4 address in dotted-quad format.
+ * @return {*} (ipv6Address) - An IPv6 address string or null if a run-time problem was detected.
+ */
 
 class IpAddress {
   constructor() {
-
+    // IAP's global log object is used to output errors, warnings, and other
+    // information to the console, IAP's log files, or a Syslog server.
+    // For more information, consult the Log Class guide on the Itential
+    // Developer Hub https://developer.itential.io/ located
+    // under Documentation -> Developer Guides -> Log Class Guide
     log.info('Starting the IpAddress product.');
   }
-        getFirstIpAddress(cidrStr, callback); {
+  getFirstIpAddress(cidrStr, callback) {
 
   // Initialize return arguments for callback
  // let firstIpAddress = null;
@@ -52,7 +67,6 @@ class IpAddress {
   if (!cidr.isValid()) {
     // If the passed CIDR is invalid, set an error message.
     callbackError = 'Error: Invalid CIDR passed to getFirstIpAddress.';
-	firstIpAddress = { 'ipv4': null, 'ipv6': null }
   } else {
     // If the passed CIDR is valid, call the object's toArray() method.
     // Notice the destructering assignment syntax to get the value of the first array's element.
@@ -65,5 +79,5 @@ class IpAddress {
   // data as the second argument to the callback function.
   return callback(firstIpAddress, callbackError);
 }
-
- module.exports = new IpAddress;
+}
+module.exports = new IpAddress;
